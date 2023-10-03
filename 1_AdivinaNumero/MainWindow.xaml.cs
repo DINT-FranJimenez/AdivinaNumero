83253dca668c098827b9086038f307fc283fe1be
alumno <alumno@A301PC33>
2023-10-03 09:06:03 +0200

    Agregar archivos de proyecto.

---
+++ 1_AdivinaNumero/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_AdivinaNumero
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static Random aletorio = new Random();
+        private static int num = aletorio.Next(0,101);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComprobarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (num == Convert.ToInt32(resultadoTextBox.Text))
+                respuestaTextBlock.Text = "Lo has adivinado!";
+            else if (Convert.ToInt32(resultadoTextBox.Text) > num)
+                respuestaTextBlock.Text = "Te has pasado de largo";
+            else if (Convert.ToInt32(resultadoTextBox.Text) < num)
+                respuestaTextBlock.Text = "Te has quedado corto";
+        }
+
+        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultadoTextBox.Text = "";
+            num = aletorio.Next(0, 101);
+        }
+    }
+}
